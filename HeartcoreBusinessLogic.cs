5f7e61a0baa233cf60c8cf721ce27622b807d5fb
meynguy <meynguy@users.noreply.github.com>
2022-07-26 21:08:47 +0200

    Update HeartcoreBusinessLogic.cs

--- HeartcoreBusinessLogic.cs
+++ HeartcoreBusinessLogic.cs
@@ -5,16 +5,18 @@ namespace MyHeartcoreLangauges
 	public static class HeartcoreBusinessLogic
 	{
 		public static readonly string ProjectAlias = "mltlngl-wbsts-sng-mbrc-hrtcr";
+		public static readonly string LanguageLibraryAlias = "languageLibrary";
+		public static readonly string ISOCodeAlias = "iSOCode";
 
 		public static Dictionary<string, string> GetAllLangaugesInLangaugeSpecificName()
 		{
 			var service = new ContentDeliveryService(ProjectAlias);
-			var languageLibraryNode = service.Content.GetRoot().Result.First(n => n.ContentTypeAlias == "languageLibrary");
+			var languageLibraryNode = service.Content.GetRoot().Result.First(n => n.ContentTypeAlias == LanguageLibraryAlias);
 
 			var returnValue = new Dictionary<string, string>();
 			foreach (var language in service.Content.GetChildren(languageLibraryNode.Id).Result.Content.Items)
 			{
-				var langIsoCode = language.Properties.First(p => p.Key == "iSOCode").Value.ToString();
+				var langIsoCode = language.Properties.First(p => p.Key == ISOCodeAlias).Value.ToString();
 				returnValue.Add(key:langIsoCode , value: service.Content.GetById(language.Id, culture: langIsoCode).Result.Name);
 			}
 			return returnValue;
@@ -23,13 +25,12 @@ namespace MyHeartcoreLangauges
 		public static Dictionary<string, string> GetAllSupportedLangagues(string selectedLanguageIsoCode)
 		{
 			var service = new ContentDeliveryService(ProjectAlias);
-			var languageLibraryNode = service.Content.GetRoot().Result.First(n => n.ContentTypeAlias == "languageLibrary");
+			var languageLibraryNode = service.Content.GetRoot().Result.First(n => n.ContentTypeAlias == LanguageLibraryAlias);
 
 			var returnValue = new Dictionary<string, string>();
 			foreach (var language in service.Content.GetChildren(languageLibraryNode.Id, culture: selectedLanguageIsoCode).Result.Content.Items)
 			{
-				var langIsoCode = language.Properties.First(p => p.Key == "iSOCode").Value.ToString();
-				returnValue.Add(key: langIsoCode, value: language.Name);
+				returnValue.Add(key: language.Properties.First(p => p.Key == ISOCodeAlias).Value.ToString(), value: language.Name);
 			}
 			return returnValue;
 		}
