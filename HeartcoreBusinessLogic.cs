ea3703176aebcd92b416ce53762603ed568237a0
meynguy <meynguy@users.noreply.github.com>
2022-07-26 18:49:26 +0200

    Update HeartcoreBusinessLogic.cs

--- HeartcoreBusinessLogic.cs
+++ HeartcoreBusinessLogic.cs
@@ -1,15 +1,37 @@
-﻿namespace MyHeartcoreLangauges
+using Umbraco.Headless.Client.Net.Delivery;
+
+namespace MyHeartcoreLangauges
 {
 	public static class HeartcoreBusinessLogic
 	{
+		public static readonly string ProjectAlias = "mltlngl-wbsts-sng-mbrc-hrtcr";
+
 		public static Dictionary<string, string> GetAllLangaugesInLangaugeSpecificName()
 		{
-			throw new NotImplementedException(); // return new Dictionary<string, string>() { { "en", "English" }, { "da", "dansk" } };
+			var service = new ContentDeliveryService(ProjectAlias);
+			var languageLibraryNode = service.Content.GetRoot().Result.First(n => n.ContentTypeAlias == "languageLibrary");
+
+			var returnValue = new Dictionary<string, string>();
+			foreach (var language in service.Content.GetChildren(languageLibraryNode.Id).Result.Content.Items)
+			{
+				var langIsoCode = language.Properties.First(p => p.Key == "iSOCode").Value.ToString();
+				returnValue.Add(key:langIsoCode , value: service.Content.GetById(language.Id, culture: langIsoCode).Result.Name);
+			}
+			return returnValue;
 		}
 
 		public static Dictionary<string, string> GetAllSupportedLangagues(string selectedLanguageIsoCode)
 		{
-			throw new NotImplementedException(); // return new Dictionary<string, string>() { { "en", "English" }, { "da", "Danish" } };
+			var service = new ContentDeliveryService(ProjectAlias);
+			var languageLibraryNode = service.Content.GetRoot().Result.First(n => n.ContentTypeAlias == "languageLibrary");
+
+			var returnValue = new Dictionary<string, string>();
+			foreach (var language in service.Content.GetChildren(languageLibraryNode.Id, culture: selectedLanguageIsoCode).Result.Content.Items)
+			{
+				var langIsoCode = language.Properties.First(p => p.Key == "iSOCode").Value.ToString();
+				returnValue.Add(key: langIsoCode, value: language.Name);
+			}
+			return returnValue;
 		}
 	}
 }
