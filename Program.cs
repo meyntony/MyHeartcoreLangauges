8faa1414980ace2bc939ca43516df7dd12f0979d
meynguy <meynguy@users.noreply.github.com>
2022-07-27 09:11:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,17 +10,25 @@ foreach (var language in allLangaugesInLangaugeSpecificName)
 { 
 	Console.WriteLine($"ISO Code: {language.Key} = {language.Value}");
 }
-string selectedLanguageIsoCode = null;
-do {
-	Console.WriteLine("Pick and type the ISO Code of the langauge you want to select from the list above: ");
-	selectedLanguageIsoCode = Console.ReadLine();
-}while (!allLangaugesInLangaugeSpecificName.ContainsKey(selectedLanguageIsoCode));
-
-Dictionary<string, string> allSupportedLangagues = HeartcoreBusinessLogic.GetAllSupportedLangagues(selectedLanguageIsoCode);
-var service = new ContentDeliveryService(HeartcoreBusinessLogic.ProjectAlias);
-var welcomeMessage = service.Content.GetRoot(culture: selectedLanguageIsoCode).Result.First(n => n.ContentTypeAlias == "home").Properties.First(p => p.Key == "welcomeMessage").Value;
-Console.WriteLine(welcomeMessage);
-foreach (var language in allSupportedLangagues)
+while (true)
 {
-	Console.WriteLine($"{language.Value}");
+	string selectedLanguageIsoCode = null;
+	do
+	{
+		Console.WriteLine("Pick and type the ISO Code of the langauge you want to select from the list above: ");
+		selectedLanguageIsoCode = Console.ReadLine();
+	} while (!allLangaugesInLangaugeSpecificName.ContainsKey(selectedLanguageIsoCode));
+
+	Dictionary<string, string> allSupportedLangagues = HeartcoreBusinessLogic.GetAllSupportedLangagues(selectedLanguageIsoCode);
+	var service = new ContentDeliveryService(HeartcoreBusinessLogic.ProjectAlias);
+	var welcomeMessage = service.Content.GetRoot(culture: selectedLanguageIsoCode).Result.FirstOrDefault(n => n.ContentTypeAlias == "home")?.Properties?.FirstOrDefault(p => p.Key == "welcomeMessage").Value;
+	if (welcomeMessage != null)
+	{
+		Console.WriteLine(welcomeMessage);
+	}
+	foreach (var language in allSupportedLangagues)
+	{
+		Console.WriteLine($"{language.Value}");
+	}
 }
+
